2a38d55ef82f8fe0fabba2514cc1c2bc48138ffa
konohairoha <78608628+konohairoha@users.noreply.github.com>
2021-02-13 15:29:59 +0900

    バグ修正と一部改善

--- src/main/java/sweetmagic/init/item/sm/magic/MFStuff.java
+++ src/main/java/sweetmagic/init/item/sm/magic/MFStuff.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,8 +12,8 @@ import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
@@ -22,23 +23,23 @@ import sweetmagic.init.item.sm.sweetmagic.SMItem;
 
 public class MFStuff extends SMItem {
 
-	public int magiaflux;
-
 	public MFStuff (String name) {
 		super(name);
 	}
 
 	// ブロックを右クリック
-	public EnumActionResult useStack (World world, BlockPos pos, EntityPlayer player, ItemStack stack) {
+	@Override
+	public EnumActionResult useStack (World world, EntityPlayer player, ItemStack stack, BlockPos pos, EnumFacing face) {
 
+		System.out.println("==" + world.getActualHeight());
+		System.out.println("==" + world.getTopSolidOrLiquidBlock(new BlockPos(pos.getX(), 0, pos.getY())));
 		if (player.isSneaking()) { return EnumActionResult.PASS; }
 
 		NBTTagCompound tags = stack.getTagCompound();
 		if (tags == null || !tags.hasKey("X")) { return EnumActionResult.PASS; }
 
 		if (!world.isRemote) {
-			String tip = new TextComponentTranslation("tip.posremo.name", new Object[0]).getFormattedText();
-			player.sendMessage(new TextComponentString(tip));
+			player.sendMessage(new TextComponentTranslation("tip.posremo.name", new Object[0]));
 		} else {
 			player.playSound(SoundEvents.ENTITY_ENDERMEN_TELEPORT, 1, 1);
 		}
@@ -53,14 +54,22 @@ public class MFStuff extends SMItem {
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag advanced) {
 
+		NBTTagCompound tags = stack.getTagCompound();
 		String click = new TextComponentTranslation("tip.mfstuff.name", new Object[0]).getFormattedText();
 		tooltip.add(I18n.format(TextFormatting.RED + click));
 
-		NBTTagCompound tags = stack.getTagCompound();
-
 		if (tags != null && tags.hasKey("X")) {
+
 			String pos = new TextComponentTranslation("tip.pos.name", new Object[0]).getFormattedText();
-			tooltip.add(I18n.format(TextFormatting.GREEN + pos + " : " + tags.getInteger("X") + ", " + tags.getInteger("Y") + ", " + tags.getInteger("Z")));
+			int x = tags.getInteger("X");
+			int y = tags.getInteger("Y");
+			int z = tags.getInteger("Z");
+			tooltip.add(I18n.format(TextFormatting.GREEN + pos + " : " + x + ", " + y + ", " + z));
+
+			Block block = world.getBlockState(new BlockPos(x, y, z)).getBlock();
+			String blockTip = new TextComponentTranslation(block.getUnlocalizedName() + ".name", new Object[0]).getFormattedText();
+			String tip = new TextComponentTranslation("tip.registerblock.name", new Object[0]).getFormattedText();
+			tooltip.add(I18n.format(TextFormatting.GREEN + tip + " ： " + blockTip));
 		}
 	}
 }
